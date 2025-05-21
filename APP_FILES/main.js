dc21cdfb0a211d79677cf503ab81e976e5fbb7d4
Dristi Roy <dristiroy@gmail.com>
2025-05-21 14:34:04 -0700

    configured drive and calendar

---
+++ APP_FILES/main.js
@@ -0,0 +1,41 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  mainWindow.loadFile('index.html');
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  // Quit the app after 20 seconds
+  setTimeout(() => {
+    console.log("20 seconds elapsed. Quitting the app...");
+    app.quit();
+  }, 20000);
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
